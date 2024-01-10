17923d96ad25ef6b9452dda4588bf70243d8a78d
TMY-0705 <148175126+TMY-0705@users.noreply.github.com>
2024-01-10 12:42:28 +0900

    Update CLEATE TABLE.sql

--- sql/CLEATE TABLE.sql
+++ sql/CLEATE TABLE.sql
@@ -1,4 +1,19 @@
 CREATE TABLE music_table (
     music_id INT AUTO_INCREMENT PRIMARY KEY,
     music_name VARCHAR(100),
-)
+    artist_name VARCHAR(50)
+);
+
+INSERT INTO music_table (music_id, music_name, artist_name) VALUES
+(1, 'ふたりごと', 'RADWIMPS'),
+(2, '有心論', 'RADWIMPS'),
+(3, 'トレモロ', 'RADWIMPS'),
+(4, 'soranji', 'Mrs.GREEN APPLE'),
+(5, 'ケセラセラ', 'Mrs.GREEN APPLE'),
+(6, '僕のこと', 'Mrs.GREEN APPLE'),
+(7, '祈り花', '平井大'),
+(8, 'Romeo+Juliet: Love goes on', '平井大'),
+(9, '題名のない今日', '平井大'),
+(10, 'しわあわせ', 'Vaundy'),
+(11, '不可幸力', 'Vaundy'),
+(12, 'おもかげ', 'Vaundy'),
