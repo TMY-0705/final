8b12dccaee5e2370a42bd3de2eced407d8a88bb6
TMY-0705 <148175126+TMY-0705@users.noreply.github.com>
2024-01-15 11:41:41 +0900

    a

--- src/musicselect.php
+++ src/musicselect.php
@@ -14,7 +14,7 @@ $pdo=new PDO('mysql:host=mysql220.phy.lolipop.lan;dbname=LAA1517434-final;charse
 
 echo '<h1>音楽一覧</h1> <a href="./top.php">HOME</a> <hr>'; echo '<div>'; 
 foreach ($pdo->query('SELECT music.*, category.category_name FROM music LEFT JOIN category ON music.category_id = category.category_id') as $row) { 
-    echo '<div class="list"> <img id="img" src="../img/', $row['img'], '"> <p id="mname">', $row['music_name'], '</p> <a id="sname">楽曲.', $row['singer_name'], '</a> <p id="category">カテゴリ: ', $row['category_name'], '</p> </div>'; }
+    echo '<div class="list"> <img id="img" src="../img/', $row['img'], '"> <p id="mname">', $row['music_name'], '</p> <a id="sname">楽曲.', $row['artist_name'], '</a> <p id="category">カテゴリ: ', $row['category_name'], '</p> </div>'; }
     echo '</div>'; 
     ?>
 
